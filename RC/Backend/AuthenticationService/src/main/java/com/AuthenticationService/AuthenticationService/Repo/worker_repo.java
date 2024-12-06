0c107ead109755d04e02167de26828188767da06
MedEZZOUAK <ezzouak2001@gmail.com>
2024-12-06 18:21:50 +0100

    fixed run prob

--- RC/Backend/AuthenticationService/src/main/java/com/AuthenticationService/AuthenticationService/Repo/worker_repo.java
+++ RC/Backend/AuthenticationService/src/main/java/com/AuthenticationService/AuthenticationService/Repo/worker_repo.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface worker_repo extends JpaRepository<worker, Float> {
-  boolean existsBy(String email);
-
   boolean existsByMatriculation(String n_immatriculation);
 
   worker findByMatriculation(String nImmatriculation);
