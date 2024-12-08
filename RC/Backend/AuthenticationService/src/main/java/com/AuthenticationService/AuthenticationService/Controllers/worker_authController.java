beb65586d04f9102ea0dfdc5c61bec5131d5dabf
Ayoub-Akb <ayoubakoubri@gmail.com>
2024-12-09 00:31:37 +0100

    Docker setup done

--- RC/Backend/AuthenticationService/src/main/java/com/AuthenticationService/AuthenticationService/Controllers/worker_authController.java
+++ RC/Backend/AuthenticationService/src/main/java/com/AuthenticationService/AuthenticationService/Controllers/worker_authController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/worker")
+@RequestMapping("/authenticate/worker")
 @RequiredArgsConstructor
 @Controller
 public class worker_authController {
